cf058f0fc30cd7b7b1fc532abd9a105f5a4738c7
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-04-01 12:28:29 +0200

    Responsitivity issue solved

--- musicplayer/src/components/Song.js
+++ musicplayer/src/components/Song.js
@@ -6,24 +6,41 @@ import holder from "../assets/holder.png";
 
 const Song = ({ currentSong, isPlaying, songInfo }) => {
   return (
-    <div  className="song-container">
+    <div className="song-container">
       <div className="turntable-container">
         <img className="turntable" src={table} alt="turntable" />
         <img
           className={`rod ${isPlaying ? "" : "notPlaying"}`}
-          style={ isPlaying ? { transform: `translateY(-7%) rotateZ(${23+songInfo.animationPercentage/100*20}deg)`} : {}}
-          src={rod} alt="rod"  />
+          style={
+            isPlaying
+              ? {
+                  transform: `translateY(-7%) rotateZ(${
+                    23 + (songInfo.animationPercentage / 100) * 20
+                  }deg)`,
+                }
+              : {}
+          }
+          src={rod}
+          alt="rod"
+        />
         <img className="holder" src={holder} alt="holder" />
         <div
           className={`cover-container ${isPlaying ? "playing" : " "}`}
-          style={ isPlaying ? { transform: `rotateZ(${720*songInfo.animationPercentage/100}deg)`} : {}}
+          style={
+            isPlaying
+              ? {
+                  transform: `rotateZ(${
+                    (720 * songInfo.animationPercentage) / 100
+                  }deg)`,
+                }
+              : {}
+          }
         >
           <img
             className="cover"
             src={currentSong.cover}
             alt={`${currentSong.name} - ${currentSong.artist}`}
-          >
-          </img>
+          ></img>
           <div className="song-info">
             <h2>{currentSong.name}</h2>
             <h3>{currentSong.artist}</h3>
